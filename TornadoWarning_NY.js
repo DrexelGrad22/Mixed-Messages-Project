5e58a3b490f7f9716219b2f0646e1329a6f469ce
Philly Dragon <afitch19104@gmail.com>
2021-08-15 14:59:44 -0400

    Adding in a random time

--- TornadoWarning_NY.js
+++ TornadoWarning_NY.js
@@ -7,4 +7,7 @@ numCities = cities.length;
 let randInt2 = Math.floor(Math.random()*numCities);
 let randCity2 = cities[randInt2];
 console.log(randCity1,randCity2);
-console.log(`Alert! a tornado warning is in effect for the following cities: ${randCity1}, ${randCity2}`);
+let times = ['1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00'];
+let randInt3 = Math.floor(Math.random()*times.length);
+let time = times[randInt3];
+console.log(`Alert! a tornado warning is in effect for the following cities: ${randCity1}, ${randCity2} at ${time}`);
